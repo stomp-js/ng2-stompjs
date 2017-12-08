a08a9dbb6f970c8f59012bbd0e09155f94c3d3c6
deepak <deepak@kreatio.com>
2017-12-08 15:23:21 +0530

    Generated doc

--- docs/assets/js/search.js
+++
@@ -1,3 +0,0 @@
-﻿var typedoc = typedoc || {};
-            typedoc.search = typedoc.search || {};
-            typedoc.search.data = {"kinds":{"65536":"Type literal"},"rows":[{"id":0,"kind":65536,"name":"__type","url":"globals.html#stompheaders.__type","classes":"tsd-kind-type-literal tsd-parent-kind-type-alias tsd-is-not-exported","parent":"StompHeaders"}]};
