4194c74d42cc0fe0d177f36f50f6da82bf4eef34
deepak <deepak@kreatio.com>
2018-11-01 22:15:29 +0530

    Enhanced test cases

--- src/specs/app/services/rx-stomp-service.spec.ts
+++ src/specs/app/services/rx-stomp-service.spec.ts
@@ -1,4 +1,4 @@
-import {TestBed, inject} from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 import {take} from "rxjs/operators";
 import {defaultRxStompConfig, disconnectRxStompAndEnsure} from "./rx-helpers";
 import {RxStompService} from "../../../rx-stomp.service";
@@ -7,35 +7,66 @@ import {InjectableRxStompConfig, rxStompServiceFactory} from "../../../../";
 describe('RxStompService', () => {
   let rxStompService: RxStompService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [
-        {
-          provide: RxStompService,
-          useFactory: rxStompServiceFactory,
-          deps: [InjectableRxStompConfig]
-        },
-        {
-          provide: InjectableRxStompConfig,
-          useValue: defaultRxStompConfig
-        }
-      ]
+  describe('Raw', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        providers: [
+          RxStompService
+        ]
+      });
+
+      rxStompService = TestBed.get(RxStompService);
     });
 
-    rxStompService = TestBed.get(RxStompService);
+    afterEach((done) => {
+      disconnectRxStompAndEnsure(rxStompService, done);
+    });
+
+    it('should be created', () => {
+      expect(rxStompService).toBeTruthy();
+    });
+
+    it('should connect', (done) => {
+      rxStompService.configure(defaultRxStompConfig);
+      rxStompService.activate();
+
+      rxStompService.connected$.pipe(take(1)).subscribe(() => {
+        done()
+      });
+    });
   });
 
-  afterEach((done) => {
-    disconnectRxStompAndEnsure(rxStompService, done);
-  });
+  describe('Pre configured', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        providers: [
+          {
+            provide: RxStompService,
+            useFactory: rxStompServiceFactory,
+            deps: [InjectableRxStompConfig]
+          },
+          {
+            provide: InjectableRxStompConfig,
+            useValue: defaultRxStompConfig
+          }
+        ]
+      });
 
-  it('should be created', () => {
-    expect(rxStompService).toBeTruthy();
-  });
+      rxStompService = TestBed.get(RxStompService);
+    });
 
-  it('should connect', (done) => {
-    rxStompService.connected$.pipe(take(1)).subscribe(() => {
-      done()
+    afterEach((done) => {
+      disconnectRxStompAndEnsure(rxStompService, done);
+    });
+
+    it('should be created', () => {
+      expect(rxStompService).toBeTruthy();
+    });
+
+    it('should connect', (done) => {
+      rxStompService.connected$.pipe(take(1)).subscribe(() => {
+        done()
+      });
     });
   });
 
