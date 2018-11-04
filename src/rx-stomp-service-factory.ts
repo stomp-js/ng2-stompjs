0fcdedbb1e6ebb53967c04bc26e78827047cfb25
deepak <deepak@kreatio.com>
2018-11-04 23:13:48 +0530

    Fix error

--- src/rx-stomp-service-factory.ts
+++ src/rx-stomp-service-factory.ts
@@ -1,11 +1,10 @@
 import {InjectableRxStompConfig} from './injectable-rx-stomp-config';
 import {RxStompService} from './rx-stomp.service';
-import {TestBed} from '@angular/core/testing';
 
 export function rxStompServiceFactory(rxStompConfig: InjectableRxStompConfig): RxStompService {
   const rxStompService = new RxStompService();
 
-  rxStompService.configure(TestBed.get(InjectableRxStompConfig));
+  rxStompService.configure(rxStompConfig);
   rxStompService.activate();
 
   return rxStompService;
