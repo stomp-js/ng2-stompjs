62b7e83d1376609536591ea74b9681d859cfe75c
Deepak Kumar <deepak@kreatio.com>
2020-10-10 11:03:17 +0530

    Typo in documentation

--- src/app/compatibility/stomp-r.service.ts
+++ src/app/compatibility/stomp-r.service.ts
@@ -174,7 +174,7 @@ export class StompRService extends RxStomp {
    *
    * Note that when working with temporary queues where the subscription request
    * creates the
-   * underlying queue, mssages might be missed during reconnect. This issue is not specific
+   * underlying queue, messages might be missed during reconnect. This issue is not specific
    * to this library but the way STOMP brokers are designed to work.
    *
    * @param queueName
